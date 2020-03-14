e97dda6ab4d41011827c225d123b9f4abffed9e3
katherinebl <kath.gbl@gmail.com>
2020-03-14 22:52:54 +0100

    Attribute Binding

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = new Vue({
   el: '#app',
   data: {
     product: 'Vueee',
-    description: 'Vue is a progressive framework and is also super easy to learn :)'
+    description: 'Vue is a progressive framework and is also super easy to learn :)',
+    imageSrc: 'img/vue-green.jpeg',
+    altName: 'Vue Logo Green',
+    url: 'https://vuejs.org/'
   }
 });
