845d0b1b00a6e45d2db769a8a932255514e234da
katherinebl <kath.gbl@gmail.com>
2020-03-15 19:54:23 +0100

    List Rendering

--- main.js
+++ main.js
@@ -7,6 +7,18 @@ const app = new Vue({
     altName: 'Vue Logo Green',
     url: 'https://vuejs.org/',
     inventory: 100,
-    onSale: true
+    onSale: false,
+    details: ["40 hours course", "100 spots", "Available online"],
+    variants: [
+      {
+        variantID: 1,
+        variantColor: 'Green'
+      },
+      {
+        variantID: 2,
+        variantColor: 'Yellow'
+      }
+    ],
+    sizes: [20, 50, 80]
   }
 });
