2c4be34983750e512f1a297a2d67cc2a4e899c7f
katherinebl <kath.gbl@gmail.com>
2020-04-10 18:47:07 +0200

    Register Vue Component and reused several times

--- main.js
+++ main.js
@@ -1,30 +1,79 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Loguis',
-    description: 'Vue is a progressive framework and is also super easy to learn :)',
-    selectedVariant: 0,
-    altName: 'Vue Logo Green',
-    url: 'https://vuejs.org/',
-    details: ["40 hours course", "100 spots", "Available online"],
-    variants: [
-      {
-        variantID: 1,
-        variantColor: 'green',
-        variantImage: 'img/vue-green.jpeg',
-        variantQuantity: 10
-      },
-      {
-        variantID: 2,
-        variantColor: 'yellow',
-        variantImage: 'img/vue-yellow.png',
-        variantQuantity: 0
+// Register Main Component
+Vue.component('product', {
+  template: `
+    <div class="product">
+
+      <div class="product-image">
+        <a :href="url">
+          <img :src="image" :alt="altName">
+        </a>
+      </div>
+
+      <div class="product-info">
+        <h1>{{title}}</h1>
+
+        <p>{{description}}</p>
+
+        <ul>
+          <li v-for="detail in details">{{detail}}</li>
+        </ul>
+
+        <div class="color-box" 
+            v-for="(variant, index) in variants" 
+            :key="variant.variantID"
+            :style="{backgroundColor: variant.variantColor}"
+            @mouseover="updateProduct(index)"
+        >
+        </div>
+
+        <p v-if="!inStock" :style="{color: '#FF0000'}">Out of Stock :(</p>
+        <p v-else :style="{color: '#84CF6A'}">Available! :)</p>
+
+
+        <button @click="addToCart"
+                :disabled ="!inStock"
+                :class="{disabledButton: !inStock}"
+        >Add to Cart</button>
+
+        <button @click="removeFromCart" 
+                :disabled = "cart == 0"
+                :class="{disabledButton: cart == 0}"
+        >Remove</button>
+
+        <div class="cart">
+          <p>Cart( {{cart}} )</p>
+        </div>
+      </div>
+
+    </div>
+  `,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Loguis',
+      description: 'Vue is a progressive framework and is also super easy to learn :)',
+      selectedVariant: 0,
+      altName: 'Vue Logo Green',
+      url: 'https://vuejs.org/',
+      details: ["40 hours course", "100 spots", "Available online"],
+      variants: [
+        {
+          variantID: 1,
+          variantColor: 'green',
+          variantImage: 'img/vue-green.jpeg',
+          variantQuantity: 10
+        },
+        {
+          variantID: 2,
+          variantColor: 'yellow',
+          variantImage: 'img/vue-yellow.png',
+          variantQuantity: 0
+        }
+      ],
+      cart: 0,
+      disabledButton: {
+        backgroundColor: 'grey'
       }
-    ],
-    cart: 0,
-    disabledButton: {
-      backgroundColor: 'grey'
     }
   },
   methods: {
@@ -51,4 +100,8 @@ const app = new Vue({
       return this.variants[this.selectedVariant].variantQuantity;
     }
   }
+})
+
+const app = new Vue({
+  el: '#app', 
 });
